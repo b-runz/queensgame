a030ca7c1aa6eaecc6ff15f0e414ce6c2d9bed02
b-runz <7459484+b-runz@users.noreply.github.com>
2026-01-31 07:50:17 +0000

    feat: add level 641

---
+++ src/utils/levels/level641.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  saharaSand,
+  lightWisteria,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level641 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "C", "C", "C", "D"],
+    ["A", "B", "B", "B", "B", "C", "E", "C", "D"],
+    ["A", "B", "B", "B", "D", "C", "E", "C", "D"],
+    ["A", "A", "A", "B", "D", "C", "C", "C", "D"],
+    ["B", "B", "B", "B", "D", "D", "D", "D", "D"],
+    ["F", "G", "G", "G", "D", "D", "H", "D", "D"],
+    ["F", "G", "I", "G", "D", "D", "H", "D", "D"],
+    ["F", "G", "I", "G", "D", "D", "H", "D", "D"],
+    ["F", "G", "G", "G", "D", "D", "H", "H", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: saharaSand,
+    G: lightWisteria,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level641;
