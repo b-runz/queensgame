4a4b9ebe7159226107d63bfb44883f551963d2b4
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-03 08:31:41 +0000

    feat: add level 644

---
+++ src/utils/levels/level644.ts
@@ -0,0 +1,33 @@
+import {
+  saharaSand,
+  lightWisteria,
+  altoMain,
+  chardonnay,
+  anakiwa,
+  bittersweet,
+  celadon
+} from "../colors";
+
+const level644 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "A", "A", "A", "A", "A"],
+    ["A", "B", "C", "C", "A", "A", "A"],
+    ["D", "D", "C", "C", "A", "A", "A"],
+    ["D", "D", "E", "E", "E", "A", "A"],
+    ["D", "D", "F", "F", "G", "G", "A"],
+    ["D", "D", "F", "F", "G", "G", "A"]
+  ],
+  regionColors: {
+    A: saharaSand,
+    B: lightWisteria,
+    C: altoMain,
+    D: chardonnay,
+    E: anakiwa,
+    F: bittersweet,
+    G: celadon
+  },
+};
+
+export default level644;
