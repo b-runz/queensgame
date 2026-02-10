c0559c9285c6bf8ac408c152d1a5b37d8976566c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-10 08:31:24 +0000

    feat: add level 651

---
+++ src/utils/levels/level651.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level651 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "C", "C", "C"],
+    ["A", "A", "D", "E", "E", "C", "C"],
+    ["A", "D", "D", "E", "E", "E", "C"],
+    ["D", "D", "D", "F", "E", "E", "G"],
+    ["D", "D", "F", "F", "F", "E", "G"],
+    ["D", "F", "F", "F", "F", "F", "G"],
+    ["F", "F", "F", "F", "F", "F", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level651;
