92c311698ec90e778594d1bcc4a1db1aacc9cfb6
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-11 08:31:13 +0000

    feat: add level 652

---
+++ src/utils/levels/level652.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  lightWisteria,
+  saharaSand,
+  bittersweet,
+  celadon,
+  chardonnay,
+  emerald
+} from "../colors";
+
+const level652 = {
+  size: 8,
+  colorRegions: [
+    ["A", "B", "C", "C", "C", "C", "C", "C"],
+    ["B", "B", "B", "D", "E", "E", "C", "C"],
+    ["F", "B", "D", "D", "E", "E", "C", "C"],
+    ["F", "D", "D", "E", "E", "E", "E", "C"],
+    ["F", "D", "E", "E", "E", "E", "E", "C"],
+    ["F", "E", "E", "F", "E", "E", "G", "C"],
+    ["F", "E", "E", "F", "E", "G", "G", "G"],
+    ["F", "F", "F", "F", "F", "F", "G", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: lightWisteria,
+    D: saharaSand,
+    E: bittersweet,
+    F: celadon,
+    G: chardonnay,
+    H: emerald
+  },
+};
+
+export default level652;
