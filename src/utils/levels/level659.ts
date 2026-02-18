74d2f2b096f0c48bbf5db79c5f9bdfc4627ae5ea
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-18 08:31:19 +0000

    feat: add level 659

---
+++ src/utils/levels/level659.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level659 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "C"],
+    ["A", "D", "D", "D", "D", "D", "C"],
+    ["A", "D", "E", "F", "F", "D", "C"],
+    ["A", "D", "E", "F", "E", "D", "G"],
+    ["A", "D", "E", "E", "E", "D", "G"],
+    ["A", "D", "D", "D", "D", "D", "G"],
+    ["G", "G", "G", "G", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level659;
