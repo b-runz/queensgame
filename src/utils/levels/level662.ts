29d2c8329f5d1815d0445da5b796e6ca48505cbd
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-21 08:31:15 +0000

    feat: add level 662

---
+++ src/utils/levels/level662.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  lightWisteria,
+  anakiwa,
+  bittersweet,
+  chardonnay,
+  celadon,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level662 = {
+  size: 9,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "C", "C", "C", "C"],
+    ["A", "B", "D", "C", "B", "C", "C", "C", "C"],
+    ["A", "B", "D", "C", "C", "C", "E", "E", "C"],
+    ["A", "B", "B", "B", "C", "E", "E", "E", "E"],
+    ["A", "A", "A", "B", "C", "E", "F", "F", "E"],
+    ["B", "A", "A", "B", "C", "E", "F", "G", "E"],
+    ["B", "B", "B", "B", "H", "E", "G", "G", "E"],
+    ["H", "H", "H", "H", "H", "E", "E", "E", "E"],
+    ["H", "H", "H", "H", "H", "H", "E", "E", "I"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: lightWisteria,
+    C: anakiwa,
+    D: bittersweet,
+    E: chardonnay,
+    F: celadon,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level662;
