4f58b1c57a47773d81225df34da0f89f85fe534a
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-24 08:31:18 +0000

    feat: add level 665

---
+++ src/utils/levels/level665.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level665 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "C", "C", "A", "D", "D"],
+    ["A", "B", "B", "B", "A", "D", "D"],
+    ["A", "A", "A", "A", "A", "D", "D"],
+    ["D", "D", "D", "D", "D", "D", "D"],
+    ["D", "E", "F", "F", "F", "F", "F"],
+    ["D", "E", "F", "G", "F", "G", "F"],
+    ["E", "E", "F", "G", "G", "G", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level665;
