c2933ab1074eb7a588cb72adee8b3dd0e27428fd
b-runz <7459484+b-runz@users.noreply.github.com>
2026-02-26 08:31:17 +0000

    feat: add level 667

---
+++ src/utils/levels/level667.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level667 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["B", "B", "A", "A", "A", "C", "C"],
+    ["B", "D", "D", "E", "F", "F", "C"],
+    ["B", "D", "E", "E", "E", "F", "C"],
+    ["B", "D", "D", "E", "F", "F", "C"],
+    ["B", "B", "E", "E", "E", "C", "C"],
+    ["G", "E", "E", "E", "E", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level667;
