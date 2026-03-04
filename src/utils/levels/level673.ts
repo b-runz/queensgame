99d338212855bb5c57b99c6362ef74bece88c87b
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-04 08:31:19 +0000

    feat: add level 673

---
+++ src/utils/levels/level673.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald
+} from "../colors";
+
+const level673 = {
+  size: 8,
+  colorRegions: [
+    ["A", "B", "C", "C", "C", "C", "C", "C"],
+    ["B", "B", "B", "C", "C", "D", "C", "C"],
+    ["C", "B", "C", "C", "D", "D", "D", "C"],
+    ["C", "C", "C", "C", "C", "D", "C", "C"],
+    ["C", "C", "E", "C", "C", "F", "F", "F"],
+    ["C", "E", "E", "E", "C", "F", "G", "F"],
+    ["C", "C", "E", "C", "C", "G", "G", "G"],
+    ["C", "C", "C", "C", "C", "C", "G", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald
+  },
+};
+
+export default level673;
