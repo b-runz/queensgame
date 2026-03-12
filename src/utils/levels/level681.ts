64f28247482307efb98015b637156bec46569501
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-12 08:31:13 +0000

    feat: add level 681

---
+++ src/utils/levels/level681.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald
+} from "../colors";
+
+const level681 = {
+  size: 8,
+  colorRegions: [
+    ["A", "B", "C", "C", "C", "D", "D", "D"],
+    ["B", "B", "C", "C", "C", "D", "D", "D"],
+    ["B", "B", "B", "C", "C", "D", "D", "D"],
+    ["E", "E", "E", "C", "C", "C", "F", "F"],
+    ["E", "E", "E", "E", "F", "F", "F", "F"],
+    ["G", "G", "G", "F", "F", "F", "F", "F"],
+    ["G", "G", "G", "F", "F", "F", "F", "F"],
+    ["G", "G", "G", "F", "F", "F", "F", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald
+  },
+};
+
+export default level681;
