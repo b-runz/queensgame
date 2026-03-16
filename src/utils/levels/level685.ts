36828803e302f133d8c6905e0e1f1e7c1c0ce9c0
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-16 08:31:23 +0000

    feat: add level 685

---
+++ src/utils/levels/level685.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  chardonnay,
+  lightWisteria,
+  bittersweet,
+  saharaSand,
+  celadon
+} from "../colors";
+
+const level685 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "C", "C", "C", "D", "D"],
+    ["A", "B", "C", "E", "C", "D", "D"],
+    ["A", "C", "C", "C", "C", "C", "D"],
+    ["A", "A", "A", "C", "F", "F", "F"],
+    ["C", "A", "A", "C", "G", "F", "C"],
+    ["C", "A", "A", "C", "G", "G", "C"],
+    ["C", "C", "C", "C", "C", "C", "C"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: chardonnay,
+    D: lightWisteria,
+    E: bittersweet,
+    F: saharaSand,
+    G: celadon
+  },
+};
+
+export default level685;
