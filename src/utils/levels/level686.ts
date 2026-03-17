ad4f17e04b0f0132e26831040eb4c40bf3944004
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-17 08:31:14 +0000

    feat: add level 686

---
+++ src/utils/levels/level686.ts
@@ -0,0 +1,33 @@
+import {
+  saharaSand,
+  lightWisteria,
+  chardonnay,
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon
+} from "../colors";
+
+const level686 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "B", "B", "A", "A", "A"],
+    ["A", "A", "C", "C", "C", "A", "A"],
+    ["A", "D", "D", "D", "D", "A", "A"],
+    ["A", "A", "E", "E", "E", "E", "A"],
+    ["A", "F", "F", "F", "F", "A", "A"],
+    ["A", "A", "G", "G", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: saharaSand,
+    B: lightWisteria,
+    C: chardonnay,
+    D: altoMain,
+    E: anakiwa,
+    F: bittersweet,
+    G: celadon
+  },
+};
+
+export default level686;
