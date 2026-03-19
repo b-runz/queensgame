f3c2654583ed95f1acad0c01203e11e92ecbdcfa
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-19 08:31:18 +0000

    feat: add level 688

---
+++ src/utils/levels/level688.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level688 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B"],
+    ["C", "A", "B", "B", "D", "D", "B"],
+    ["C", "A", "B", "B", "B", "D", "B"],
+    ["A", "A", "A", "B", "B", "D", "B"],
+    ["E", "A", "F", "F", "D", "D", "D"],
+    ["E", "A", "F", "F", "F", "D", "G"],
+    ["E", "E", "E", "F", "F", "D", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level688;
