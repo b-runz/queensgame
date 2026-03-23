8d44b421be4ecf07f961d3392b4735c5b8c1440c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-23 08:31:11 +0000

    feat: add level 692

---
+++ src/utils/levels/level692.ts
@@ -0,0 +1,33 @@
+import {
+  lightWisteria,
+  chardonnay,
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  saharaSand
+} from "../colors";
+
+const level692 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "C"],
+    ["A", "A", "D", "B", "D", "C", "C"],
+    ["A", "D", "D", "D", "D", "D", "C"],
+    ["A", "A", "D", "E", "D", "F", "F"],
+    ["A", "D", "D", "D", "D", "D", "F"],
+    ["A", "A", "D", "G", "D", "G", "G"],
+    ["A", "A", "G", "G", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: altoMain,
+    D: anakiwa,
+    E: bittersweet,
+    F: celadon,
+    G: saharaSand
+  },
+};
+
+export default level692;
