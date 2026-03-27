f0f65dd9d0ce36c2a9d860a60655ec9dd01a5b9f
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-27 08:31:11 +0000

    feat: add level 696

---
+++ src/utils/levels/level696.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  saharaSand,
+  lightWisteria,
+  celadon,
+  chardonnay,
+  emerald
+} from "../colors";
+
+const level696 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "C", "C"],
+    ["A", "A", "B", "B", "D", "D", "C", "C"],
+    ["B", "B", "B", "B", "D", "D", "B", "B"],
+    ["B", "E", "E", "B", "B", "B", "B", "B"],
+    ["B", "E", "E", "F", "F", "F", "F", "B"],
+    ["B", "B", "B", "F", "F", "F", "F", "B"],
+    ["B", "G", "G", "F", "F", "F", "F", "B"],
+    ["B", "G", "G", "F", "F", "F", "F", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: saharaSand,
+    E: lightWisteria,
+    F: celadon,
+    G: chardonnay,
+    H: emerald
+  },
+};
+
+export default level696;
