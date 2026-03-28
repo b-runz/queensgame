a0752966bcbe7476b9b8c4cf71f21a393286ec81
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-28 08:31:14 +0000

    feat: add level 697

---
+++ src/utils/levels/level697.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level697 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "B", "B", "C", "D", "A", "A", "A"],
+    ["A", "B", "C", "C", "C", "D", "A", "A", "A"],
+    ["A", "B", "B", "B", "C", "D", "D", "D", "A"],
+    ["A", "A", "A", "B", "C", "D", "E", "D", "F"],
+    ["A", "B", "B", "B", "C", "D", "D", "D", "F"],
+    ["A", "G", "G", "C", "C", "C", "H", "H", "F"],
+    ["A", "G", "G", "G", "C", "I", "H", "H", "F"],
+    ["A", "G", "G", "G", "I", "I", "H", "H", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level697;
