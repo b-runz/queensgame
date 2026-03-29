fea3286eeedfd33ebf9115f6d37362cc6cb1b701
b-runz <7459484+b-runz@users.noreply.github.com>
2026-03-29 07:31:16 +0000

    feat: add level 698

---
+++ src/utils/levels/level698.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level698 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "C", "D", "E", "E"],
+    ["A", "A", "A", "B", "C", "C", "D", "D", "E"],
+    ["A", "A", "B", "B", "C", "C", "D", "D", "E"],
+    ["A", "F", "B", "F", "C", "G", "G", "G", "E"],
+    ["A", "F", "F", "F", "C", "C", "G", "E", "E"],
+    ["A", "F", "A", "F", "H", "G", "G", "G", "E"],
+    ["A", "A", "A", "A", "H", "H", "I", "E", "E"],
+    ["A", "A", "A", "H", "H", "H", "I", "I", "E"],
+    ["A", "A", "A", "A", "H", "H", "I", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level698;
