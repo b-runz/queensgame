cf9ef997afb13bf80023aecf7a922231b77867e3
b-runz <7459484+b-runz@users.noreply.github.com>
2026-04-07 07:36:16 +0000

    feat: add level 707

---
+++ src/utils/levels/level707.ts
@@ -0,0 +1,33 @@
+import {
+  lightWisteria,
+  altoMain,
+  anakiwa,
+  saharaSand,
+  bittersweet,
+  celadon,
+  chardonnay
+} from "../colors";
+
+const level707 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "B", "A", "A", "C", "D"],
+    ["A", "B", "D", "E", "A", "F", "D"],
+    ["G", "B", "D", "E", "A", "F", "D"],
+    ["G", "B", "D", "E", "A", "F", "D"],
+    ["G", "B", "D", "D", "F", "F", "D"],
+    ["D", "D", "D", "D", "D", "D", "D"]
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: altoMain,
+    C: anakiwa,
+    D: saharaSand,
+    E: bittersweet,
+    F: celadon,
+    G: chardonnay
+  },
+};
+
+export default level707;
