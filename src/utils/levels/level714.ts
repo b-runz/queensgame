8ef1f8d945b3db38241180c38e94b1f0268a361b
b-runz <7459484+b-runz@users.noreply.github.com>
2026-04-14 07:33:08 +0000

    feat: add level 714

---
+++ src/utils/levels/level714.ts
@@ -0,0 +1,33 @@
+import {
+  lightWisteria,
+  saharaSand,
+  altoMain,
+  anakiwa,
+  bittersweet,
+  chardonnay,
+  celadon
+} from "../colors";
+
+const level714 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "B"],
+    ["C", "A", "C", "D", "B", "D", "B"],
+    ["C", "C", "C", "D", "D", "D", "B"],
+    ["B", "B", "B", "B", "B", "B", "B"],
+    ["E", "B", "E", "F", "B", "F", "B"],
+    ["E", "E", "E", "F", "F", "F", "B"],
+    ["G", "B", "B", "B", "B", "B", "B"]
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: saharaSand,
+    C: altoMain,
+    D: anakiwa,
+    E: bittersweet,
+    F: chardonnay,
+    G: celadon
+  },
+};
+
+export default level714;
