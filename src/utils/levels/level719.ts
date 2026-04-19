398afac2bd70cd909e5fba56dbb8579bb88dd411
b-runz <7459484+b-runz@users.noreply.github.com>
2026-04-19 07:31:12 +0000

    feat: add level 719

---
+++ src/utils/levels/level719.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  saharaSand,
+  lightWisteria,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level719 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "B", "C", "A", "A", "D", "A", "A"],
+    ["A", "B", "B", "C", "C", "D", "D", "D", "D"],
+    ["A", "A", "B", "C", "C", "E", "E", "E", "D"],
+    ["F", "F", "B", "C", "C", "E", "G", "E", "D"],
+    ["F", "B", "B", "B", "C", "E", "E", "E", "D"],
+    ["F", "H", "H", "H", "I", "I", "I", "E", "D"],
+    ["F", "F", "F", "H", "I", "E", "E", "E", "I"],
+    ["F", "F", "F", "H", "I", "I", "I", "I", "I"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: saharaSand,
+    G: lightWisteria,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level719;
