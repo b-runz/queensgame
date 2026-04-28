5eedcbebbc868fc9c04faa02e9dd2c23500bc2fe
b-runz <7459484+b-runz@users.noreply.github.com>
2026-04-28 07:31:15 +0000

    feat: add level 728

---
+++ src/utils/levels/level728.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  saharaSand,
+  chardonnay,
+  anakiwa,
+  bittersweet,
+  celadon,
+  lightWisteria
+} from "../colors";
+
+const level728 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "B"],
+    ["A", "C", "C", "C", "A", "A", "B"],
+    ["C", "C", "C", "C", "C", "A", "A"],
+    ["D", "E", "E", "F", "E", "A", "A"],
+    ["D", "E", "E", "E", "E", "A", "A"],
+    ["G", "E", "E", "E", "A", "A", "A"],
+    ["G", "G", "G", "G", "G", "A", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: saharaSand,
+    C: chardonnay,
+    D: anakiwa,
+    E: bittersweet,
+    F: celadon,
+    G: lightWisteria
+  },
+};
+
+export default level728;
