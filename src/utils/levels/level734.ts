4816fac0b2e36c5e27ff47c82a298e956880ef9c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-05-04 07:31:28 +0000

    feat: add level 734

---
+++ src/utils/levels/level734.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  saharaSand,
+  anakiwa,
+  chardonnay,
+  bittersweet,
+  celadon,
+  lightWisteria
+} from "../colors";
+
+const level734 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["B", "A", "A", "A", "A", "A", "A"],
+    ["C", "C", "D", "D", "A", "A", "A"],
+    ["C", "C", "D", "D", "A", "A", "A"],
+    ["E", "E", "E", "F", "F", "A", "A"],
+    ["E", "E", "E", "F", "F", "A", "A"],
+    ["E", "E", "E", "F", "F", "A", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: saharaSand,
+    C: anakiwa,
+    D: chardonnay,
+    E: bittersweet,
+    F: celadon,
+    G: lightWisteria
+  },
+};
+
+export default level734;
