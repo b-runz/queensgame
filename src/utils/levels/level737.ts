683fb484ef5669a4584a5d9bcf64063c7a10cbed
b-runz <7459484+b-runz@users.noreply.github.com>
2026-05-07 07:31:26 +0000

    feat: add level 737

---
+++ src/utils/levels/level737.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level737 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "B"],
+    ["A", "A", "C", "C", "C", "B", "D"],
+    ["A", "C", "C", "C", "C", "C", "D"],
+    ["A", "C", "E", "E", "E", "C", "D"],
+    ["A", "C", "C", "C", "C", "C", "F"],
+    ["A", "C", "G", "G", "G", "C", "F"],
+    ["G", "G", "G", "G", "F", "F", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level737;
