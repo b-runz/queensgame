557bdf60b21d0ee39ad29964535b315441c0707b
b-runz <7459484+b-runz@users.noreply.github.com>
2026-05-11 07:32:43 +0000

    feat: add level 741

---
+++ src/utils/levels/level741.ts
@@ -0,0 +1,33 @@
+import {
+  saharaSand,
+  altoMain,
+  anakiwa,
+  bittersweet,
+  chardonnay,
+  lightWisteria,
+  celadon
+} from "../colors";
+
+const level741 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "B", "B"],
+    ["A", "C", "A", "B", "B", "B", "B"],
+    ["A", "A", "A", "A", "D", "B", "B"],
+    ["E", "E", "E", "D", "D", "D", "F"],
+    ["E", "E", "D", "D", "D", "D", "D"],
+    ["E", "E", "E", "E", "D", "G", "G"],
+    ["E", "E", "E", "D", "D", "D", "G"]
+  ],
+  regionColors: {
+    A: saharaSand,
+    B: altoMain,
+    C: anakiwa,
+    D: bittersweet,
+    E: chardonnay,
+    F: lightWisteria,
+    G: celadon
+  },
+};
+
+export default level741;
