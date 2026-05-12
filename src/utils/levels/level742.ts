67de96c713b319e9f2c30ec2bc4f527446124f37
b-runz <7459484+b-runz@users.noreply.github.com>
2026-05-12 07:31:36 +0000

    feat: add level 742

---
+++ src/utils/levels/level742.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  chardonnay,
+  lightWisteria,
+  bittersweet,
+  celadon,
+  saharaSand
+} from "../colors";
+
+const level742 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "C", "C", "C"],
+    ["D", "D", "A", "C", "C", "C", "C"],
+    ["A", "A", "A", "C", "C", "C", "C"],
+    ["E", "E", "A", "C", "C", "F", "C"],
+    ["A", "A", "A", "C", "F", "F", "F"],
+    ["C", "C", "C", "C", "C", "F", "G"],
+    ["C", "C", "C", "C", "F", "F", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: chardonnay,
+    D: lightWisteria,
+    E: bittersweet,
+    F: celadon,
+    G: saharaSand
+  },
+};
+
+export default level742;
