9ca11f4d15b256be884cef9a9073c4e49483ae22
b-runz <7459484+b-runz@users.noreply.github.com>
2026-05-27 07:31:18 +0000

    feat: add level 756

---
+++ src/utils/levels/level756.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level756 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B"],
+    ["A", "C", "C", "B", "B", "B", "B"],
+    ["B", "B", "B", "B", "D", "B", "E"],
+    ["B", "F", "F", "F", "D", "B", "E"],
+    ["B", "F", "G", "G", "D", "E", "E"],
+    ["B", "F", "F", "G", "D", "E", "E"],
+    ["B", "F", "F", "D", "D", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level756;
