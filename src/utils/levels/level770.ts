dbc0a86d4c016b58c230d6b43dbdfc3523929b50
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-10 07:31:31 +0000

    feat: add level 770

---
+++ src/utils/levels/level770.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level770 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "C", "C", "C", "A", "D"],
+    ["B", "B", "E", "C", "E", "D", "D"],
+    ["E", "B", "E", "E", "E", "E", "D"],
+    ["E", "E", "E", "E", "E", "F", "G"],
+    ["E", "E", "E", "E", "F", "F", "G"],
+    ["E", "E", "E", "E", "E", "F", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level770;
