edd87209e998cacb8e238bc2269e9cfb4d80b1b8
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-15 07:31:35 +0000

    feat: add level 775

---
+++ src/utils/levels/level775.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  saharaSand,
+  bittersweet,
+  chardonnay,
+  lightWisteria,
+  celadon
+} from "../colors";
+
+const level775 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "C", "B"],
+    ["A", "A", "A", "A", "B", "B", "B"],
+    ["D", "A", "A", "A", "A", "B", "B"],
+    ["D", "E", "A", "A", "A", "A", "A"],
+    ["E", "E", "F", "F", "A", "A", "A"],
+    ["E", "E", "F", "F", "A", "A", "A"],
+    ["E", "E", "E", "E", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: saharaSand,
+    D: bittersweet,
+    E: chardonnay,
+    F: lightWisteria,
+    G: celadon
+  },
+};
+
+export default level775;
