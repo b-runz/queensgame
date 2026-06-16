cc011da7155efbfc077ede22775363d7e64bb08c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-16 07:31:35 +0000

    feat: add level 776

---
+++ src/utils/levels/level776.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level776 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["B", "B", "B", "A", "A", "A", "C"],
+    ["D", "D", "D", "A", "A", "A", "C"],
+    ["D", "D", "D", "A", "E", "E", "E"],
+    ["D", "F", "D", "A", "E", "E", "E"],
+    ["D", "D", "D", "G", "E", "E", "E"],
+    ["D", "D", "D", "G", "E", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level776;
