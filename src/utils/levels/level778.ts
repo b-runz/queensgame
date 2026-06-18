e9d8cfb6e17193a85378395a07241a5ce9d0762c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-18 07:31:29 +0000

    feat: add level 778

---
+++ src/utils/levels/level778.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald
+} from "../colors";
+
+const level778 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "B", "B"],
+    ["A", "A", "C", "C", "D", "D", "B", "B"],
+    ["A", "C", "C", "E", "E", "D", "D", "B"],
+    ["A", "C", "E", "E", "E", "E", "D", "B"],
+    ["A", "F", "E", "E", "E", "E", "G", "B"],
+    ["A", "F", "F", "E", "E", "G", "G", "H"],
+    ["A", "A", "F", "F", "G", "G", "A", "H"],
+    ["A", "A", "A", "A", "A", "A", "A", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald
+  },
+};
+
+export default level778;
