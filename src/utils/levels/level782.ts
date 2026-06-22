176ec792959f1e35f47c651eb951c70aebaccf8b
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-22 07:31:33 +0000

    feat: add level 782

---
+++ src/utils/levels/level782.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level782 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "B", "A"],
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "C", "A", "A", "D"],
+    ["E", "A", "C", "C", "C", "D", "D"],
+    ["E", "F", "F", "F", "F", "D", "D"],
+    ["E", "E", "F", "G", "F", "D", "D"],
+    ["E", "E", "F", "F", "F", "D", "D"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level782;
