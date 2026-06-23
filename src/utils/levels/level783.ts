b1fb8b1cc5863503e8161db251d289595464de42
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-23 07:31:29 +0000

    feat: add level 783

---
+++ src/utils/levels/level783.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald
+} from "../colors";
+
+const level783 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A", "A"],
+    ["A", "A", "A", "B", "B", "B", "C", "A"],
+    ["D", "B", "B", "B", "E", "B", "C", "C"],
+    ["D", "B", "B", "B", "B", "B", "B", "C"],
+    ["D", "B", "F", "F", "F", "F", "B", "C"],
+    ["D", "D", "F", "G", "F", "F", "F", "C"],
+    ["H", "D", "F", "F", "F", "H", "H", "H"],
+    ["H", "H", "H", "H", "H", "H", "H", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald
+  },
+};
+
+export default level783;
