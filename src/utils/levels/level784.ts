af19a576d8a9626f352a5f50b45b4b95ece97d37
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-24 07:31:37 +0000

    feat: add level 784

---
+++ src/utils/levels/level784.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  lightWisteria,
+  chardonnay,
+  saharaSand
+} from "../colors";
+
+const level784 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "B", "B"],
+    ["A", "A", "A", "B", "C", "B", "B"],
+    ["A", "A", "D", "D", "C", "E", "B"],
+    ["A", "A", "D", "C", "C", "E", "B"],
+    ["A", "A", "D", "F", "G", "E", "E"],
+    ["A", "F", "F", "F", "G", "G", "G"],
+    ["A", "A", "A", "A", "A", "A", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: lightWisteria,
+    F: chardonnay,
+    G: saharaSand
+  },
+};
+
+export default level784;
