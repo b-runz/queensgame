bd0247c5a304f246bcb071581e301004877c6043
b-runz <7459484+b-runz@users.noreply.github.com>
2026-06-26 07:31:29 +0000

    feat: add level 786

---
+++ src/utils/levels/level786.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level786 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "B", "B", "A", "C", "C", "C", "A"],
+    ["D", "A", "A", "B", "A", "C", "A", "A", "A"],
+    ["D", "A", "A", "B", "A", "A", "A", "A", "A"],
+    ["D", "D", "A", "A", "A", "A", "A", "A", "E"],
+    ["A", "A", "A", "A", "A", "F", "A", "A", "E"],
+    ["G", "A", "A", "F", "F", "F", "A", "E", "E"],
+    ["G", "A", "A", "H", "A", "A", "A", "A", "A"],
+    ["G", "G", "A", "H", "A", "I", "A", "A", "A"],
+    ["A", "A", "A", "H", "H", "I", "I", "I", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level786;
