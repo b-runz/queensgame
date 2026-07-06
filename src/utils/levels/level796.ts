e9f566fac31a2f18f01c37a9f50014efb5f5de80
b-runz <7459484+b-runz@users.noreply.github.com>
2026-07-06 07:31:51 +0000

    feat: add level 796

---
+++ src/utils/levels/level796.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level796 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "B", "B"],
+    ["B", "B", "B", "C", "C", "C", "C"],
+    ["B", "D", "D", "C", "C", "C", "C"],
+    ["B", "D", "D", "D", "E", "E", "D"],
+    ["B", "D", "D", "D", "D", "D", "D"],
+    ["B", "D", "D", "D", "F", "F", "D"],
+    ["B", "D", "D", "G", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level796;
