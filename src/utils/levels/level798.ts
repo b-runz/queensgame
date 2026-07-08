90346b11fab697262878b1aaf37fd2a10c74c77d
b-runz <7459484+b-runz@users.noreply.github.com>
2026-07-08 07:31:55 +0000

    feat: add level 798

---
+++ src/utils/levels/level798.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level798 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "C", "D", "E", "C", "F"],
+    ["A", "B", "C", "D", "E", "C", "F"],
+    ["A", "B", "C", "E", "E", "C", "F"],
+    ["A", "B", "C", "C", "C", "C", "F"],
+    ["A", "B", "F", "F", "F", "F", "F"],
+    ["A", "A", "A", "A", "A", "A", "G"],
+    ["A", "A", "A", "A", "A", "A", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level798;
