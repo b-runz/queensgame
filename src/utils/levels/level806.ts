934388751ffabb26ebdf680fce85a5e2c0c385e6
b-runz <7459484+b-runz@users.noreply.github.com>
2026-07-16 07:32:04 +0000

    feat: add level 806

---
+++ src/utils/levels/level806.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  lightWisteria,
+  bittersweet,
+  celadon,
+  saharaSand,
+  chardonnay,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level806 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "B", "B", "C", "C", "D", "D", "D"],
+    ["A", "A", "B", "B", "B", "C", "C", "D", "D"],
+    ["A", "A", "B", "E", "E", "E", "E", "E", "E"],
+    ["A", "A", "E", "E", "E", "E", "E", "E", "E"],
+    ["F", "F", "E", "E", "E", "E", "E", "E", "E"],
+    ["F", "F", "E", "E", "E", "E", "E", "E", "E"],
+    ["F", "G", "E", "E", "E", "E", "E", "E", "H"],
+    ["G", "G", "G", "G", "I", "H", "H", "H", "H"],
+    ["G", "G", "G", "I", "I", "I", "H", "H", "H"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: lightWisteria,
+    D: bittersweet,
+    E: celadon,
+    F: saharaSand,
+    G: chardonnay,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level806;
