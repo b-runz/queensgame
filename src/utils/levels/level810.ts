43c10caf2301beadd3fcabaf4eff327b90eb58ec
b-runz <7459484+b-runz@users.noreply.github.com>
2026-07-20 07:32:03 +0000

    feat: add level 810

---
+++ src/utils/levels/level810.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level810 = {
+  size: 7,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "B", "B"],
+    ["B", "B", "B", "C", "C", "B", "B"],
+    ["B", "B", "B", "B", "D", "D", "D"],
+    ["B", "E", "E", "B", "B", "B", "B"],
+    ["B", "B", "F", "F", "B", "B", "B"],
+    ["B", "B", "B", "B", "B", "G", "G"],
+    ["B", "B", "B", "B", "B", "B", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level810;
