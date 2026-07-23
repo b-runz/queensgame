722e299da9da8ba360bccea353e75cbc36821b57
b-runz <7459484+b-runz@users.noreply.github.com>
2026-07-23 07:31:58 +0000

    feat: add level 812

---
+++ src/utils/levels/level812.ts
@@ -0,0 +1,36 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald
+} from "../colors";
+
+const level812 = {
+  size: 8,
+  colorRegions: [
+    ["A", "A", "A", "B", "B", "B", "B", "C"],
+    ["D", "D", "A", "B", "E", "B", "B", "C"],
+    ["A", "A", "A", "B", "E", "E", "B", "B"],
+    ["A", "E", "E", "E", "E", "F", "F", "F"],
+    ["A", "A", "A", "E", "E", "E", "E", "F"],
+    ["G", "G", "E", "E", "G", "F", "F", "F"],
+    ["H", "G", "G", "E", "G", "G", "G", "F"],
+    ["H", "G", "G", "G", "G", "F", "F", "F"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald
+  },
+};
+
+export default level812;
