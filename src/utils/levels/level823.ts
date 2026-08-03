c89db6d84bbc72f7c50cba056c08d9d9357242b9
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-03 07:31:16 +0000

    feat: add level 823

---
+++ src/utils/levels/level823.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level823 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "C", "C"],
+    ["A", "A", "B", "D", "B", "B", "C"],
+    ["A", "A", "D", "D", "D", "B", "B"],
+    ["E", "D", "D", "F", "D", "D", "D"],
+    ["E", "E", "D", "D", "D", "E", "D"],
+    ["G", "E", "E", "D", "E", "E", "E"],
+    ["G", "G", "E", "E", "E", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level823;
