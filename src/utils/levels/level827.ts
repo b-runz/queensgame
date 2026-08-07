68f5d22dc1f8956ea875cae44c778943f9deb707
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-07 07:31:31 +0000

    feat: add level 827

---
+++ src/utils/levels/level827.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  lightWisteria,
+  chardonnay,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level827 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "B", "C", "C"],
+    ["A", "D", "D", "D", "D", "D", "B", "D", "C"],
+    ["A", "D", "E", "E", "E", "D", "F", "D", "C"],
+    ["A", "D", "E", "D", "E", "D", "F", "D", "G"],
+    ["A", "D", "E", "D", "E", "D", "F", "D", "G"],
+    ["H", "D", "I", "D", "D", "D", "F", "D", "G"],
+    ["H", "D", "I", "I", "I", "F", "F", "D", "G"],
+    ["H", "D", "D", "D", "D", "D", "D", "D", "G"],
+    ["H", "H", "H", "H", "H", "H", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: lightWisteria,
+    F: chardonnay,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level827;
