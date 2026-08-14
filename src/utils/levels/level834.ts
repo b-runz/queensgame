36f5b5860bf0c11e8819235a9d953e818983cf74
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-14 07:31:27 +0000

    feat: add level 834

---
+++ src/utils/levels/level834.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  saharaSand,
+  chardonnay,
+  lightWisteria,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level834 = {
+  size: 9,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B", "B", "B"],
+    ["A", "B", "B", "B", "C", "B", "B", "B", "B"],
+    ["A", "B", "D", "C", "C", "C", "E", "B", "B"],
+    ["A", "D", "D", "D", "C", "E", "E", "F", "B"],
+    ["A", "A", "D", "G", "G", "E", "F", "F", "F"],
+    ["A", "A", "A", "H", "G", "G", "I", "F", "A"],
+    ["A", "A", "H", "H", "H", "I", "I", "I", "A"],
+    ["A", "A", "A", "H", "A", "A", "I", "A", "A"],
+    ["A", "A", "A", "A", "A", "A", "A", "A", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: saharaSand,
+    F: chardonnay,
+    G: lightWisteria,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level834;
