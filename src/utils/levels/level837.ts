6483fe952a4f2af64445f12070b90c6b91ff2223
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-17 07:31:21 +0000

    feat: add level 837

---
+++ src/utils/levels/level837.ts
@@ -0,0 +1,33 @@
+import {
+  saharaSand,
+  altoMain,
+  lightWisteria,
+  anakiwa,
+  bittersweet,
+  chardonnay,
+  celadon
+} from "../colors";
+
+const level837 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["A", "B", "C", "C", "C", "D", "A"],
+    ["A", "B", "B", "C", "D", "D", "A"],
+    ["A", "B", "E", "F", "E", "D", "A"],
+    ["A", "B", "E", "E", "E", "D", "A"],
+    ["A", "B", "G", "G", "E", "D", "G"],
+    ["A", "A", "A", "G", "G", "G", "G"]
+  ],
+  regionColors: {
+    A: saharaSand,
+    B: altoMain,
+    C: lightWisteria,
+    D: anakiwa,
+    E: bittersweet,
+    F: chardonnay,
+    G: celadon
+  },
+};
+
+export default level837;
