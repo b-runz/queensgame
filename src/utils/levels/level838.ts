6aea91cc3552201f97c7c8b3af422efe2823570c
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-18 07:31:12 +0000

    feat: add level 838

---
+++ src/utils/levels/level838.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level838 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "B", "B", "B"],
+    ["A", "A", "C", "D", "D", "E", "E"],
+    ["A", "A", "C", "E", "E", "E", "E"],
+    ["F", "A", "G", "G", "G", "E", "E"],
+    ["F", "A", "A", "A", "G", "E", "E"],
+    ["F", "A", "G", "G", "G", "E", "E"],
+    ["F", "F", "F", "E", "E", "E", "E"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level838;
