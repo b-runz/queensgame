314a077944b640beec3f3192936391a05f4a603f
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-25 07:31:15 +0000

    feat: add level 845

---
+++ src/utils/levels/level845.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level845 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "B", "B", "B", "B", "B"],
+    ["A", "A", "C", "C", "C", "C", "C"],
+    ["D", "C", "C", "E", "E", "E", "C"],
+    ["D", "C", "F", "F", "E", "E", "C"],
+    ["D", "C", "F", "E", "E", "E", "C"],
+    ["D", "C", "C", "E", "E", "C", "C"],
+    ["D", "C", "C", "C", "C", "C", "G"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level845;
