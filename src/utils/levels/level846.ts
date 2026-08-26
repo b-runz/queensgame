7438eabdf7499aca3950d62093136b3d2fd7dda1
b-runz <7459484+b-runz@users.noreply.github.com>
2026-08-26 07:31:13 +0000

    feat: add level 846

---
+++ src/utils/levels/level846.ts
@@ -0,0 +1,33 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand
+} from "../colors";
+
+const level846 = {
+  size: 7,
+  colorRegions: [
+    ["A", "A", "A", "A", "A", "A", "A"],
+    ["B", "B", "B", "B", "B", "C", "A"],
+    ["B", "D", "D", "C", "C", "C", "A"],
+    ["D", "D", "D", "C", "E", "E", "A"],
+    ["F", "F", "F", "E", "E", "A", "A"],
+    ["G", "G", "F", "E", "A", "A", "A"],
+    ["F", "F", "F", "A", "A", "A", "A"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand
+  },
+};
+
+export default level846;
