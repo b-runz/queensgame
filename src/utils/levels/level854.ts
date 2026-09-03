c8ebfdf83e0521f5e30cd2bf67da18f2c24d7fcf
b-runz <7459484+b-runz@users.noreply.github.com>
2026-09-03 07:31:24 +0000

    feat: add level 854

---
+++ src/utils/levels/level854.ts
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightWisteria,
+  saharaSand,
+  emerald,
+  halfBaked
+} from "../colors";
+
+const level854 = {
+  size: 9,
+  colorRegions: [
+    ["A", "B", "B", "B", "B", "B", "B", "B", "B"],
+    ["A", "C", "B", "B", "B", "D", "D", "D", "D"],
+    ["A", "C", "E", "B", "B", "D", "D", "D", "D"],
+    ["F", "C", "E", "B", "B", "B", "B", "B", "D"],
+    ["F", "F", "E", "B", "B", "B", "B", "B", "B"],
+    ["G", "F", "F", "F", "F", "B", "H", "B", "B"],
+    ["G", "G", "G", "G", "F", "I", "H", "B", "B"],
+    ["G", "G", "G", "G", "F", "I", "H", "B", "B"],
+    ["F", "F", "F", "F", "F", "I", "B", "B", "B"]
+  ],
+  regionColors: {
+    A: altoMain,
+    B: anakiwa,
+    C: bittersweet,
+    D: celadon,
+    E: chardonnay,
+    F: lightWisteria,
+    G: saharaSand,
+    H: emerald,
+    I: halfBaked
+  },
+};
+
+export default level854;
